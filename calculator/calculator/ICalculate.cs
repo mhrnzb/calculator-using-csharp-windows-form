e06bd6d34b9450d710b7403641338590800638fc
mehrnaz <mehrnazbg1381@gmail.com>
2023-08-16 20:06:10 +0330

    make interface

---
+++ calculator/calculator/ICalculate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    internal interface ICalculate
+    {
+        int Plus(int number1 , int number2);
+
+        int Minus (int number1, int number2);
+
+        int Multiple(int number1, int number2);
+
+        int Devide (int number1, int number2);
+
+    }
+}
